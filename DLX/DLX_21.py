8f76c9a96130f6af4621bee502af783f5667b22b
Emin <emin@berkeley.edu>
2014-09-26 13:22:16 -0700

    Uploaded files

--- DLX/DLX_21.py
+++ DLX/DLX_21.py
@@ -7,8 +7,6 @@ DLX - by GASPY
 
 Notes:
 
-    - Memory should be more minutely managed to include Stack, Heap etc
-    - It will be better to find a way to parse using binary instead of the python split()
     - More Instructions should be added as required
 """
 
@@ -25,59 +23,128 @@ def mem(n,reg=False):
         mem=mem.pop()
     return mem
 
+
 #Executing Fucntion      
 def execute(op,a,b,c):
+    global pc
     if op=='ADDI':
-        reg[a]=[reg[b]+c]
+        reg[a]=reg[b]+c
+        pc+=1
         return
     elif op=='SUBI':
-        reg[a]=[reg[0]-c]
+        reg[a]=reg[b]-c
+        pc+=1
         return
     elif op=='MULI':
-        reg[a]=[reg[b][0]*c]
+        reg[a]=reg[b]*c
+        pc+=1
         return
     elif op=='DIVI':
-        reg[a]=[reg[b][0]/c]
+        reg[a]=reg[b]/c
+        pc+=1
+        return
+    elif op=='CMPI':
+        reg[a]=reg[b]-c
+        pc+=1
         return
         
     elif op=='LDW':
-        reg[a]=[memory[b+c][0]]
+        reg[a]=memory[reg[b]+c][0]
+        pc+=1
+        return
     elif op=='STW':
-        [memory[b+c][0]]=reg[a]
-        
+        memory[reg[b]+c][0]=reg[a]
+        pc+=1
+        return
+    elif op=='BGE':
+        if reg[a]>=0:
+            pc+=c 
+        else:
+            pc+=1
+        return    
+    elif op=='BGT':
+        if reg[a]>0:
+            pc+=c 
+        else:
+            pc+=1
+        return
+    elif op=='BLT':
+        if reg[a]<0:
+            pc+=c 
+        else:
+            pc+=1
+        return
+    elif op=='BLE':
+        if reg[a]<=0:
+            pc+=c 
+        else:
+            pc+=1
+        return
+    elif op=='BR':
+        pc+=c
+        return
+    elif op=='POP':
+        reg[a]=memory[reg[b]][0]
+        reg[b]=reg[b]+c
+        pc+=1
+        return
+    elif op=='PSH':
+        reg[b]=reg[b]-c
+        memory[reg[b]][0]=reg[a]
+        pc+=1
+        return
     #F2
     if op=='ADD':
-        reg[a]=[reg[b][0]+reg[c][0]]
+        reg[a]=reg[b]+reg[c]
+        pc+=1
         return
     elif op=='SUB':
-        reg[a]=[reg[b][0]-reg[c][0]]
+        reg[a]=reg[b]-reg[c]
+        pc+=1
         return
     elif op=='MUL':
-        reg[a]=[reg[b][0]*reg[c][0]]
+        reg[a]=reg[b]*reg[c]
+        pc+=1
         return
     elif op=='DIV':
-        reg[a]=[reg[b][0]/reg[c][0]]
+        reg[a]=reg[b]/reg[c]
+        pc+=1
+        return
+    elif op=='CMP':
+        reg[a]=reg[b]-reg[c]
+        pc+=1
+        return
+    elif op=='MOV':
+        reg[a]=reg[c]
+        pc+=1
+        return
+    elif op=='RET':
+        pc=reg[c]
         return
 
+
+#Reading the code
+add='log1.txt'
+with open(add,'rb') as f:
+    read=f.readlines()
+    
 memory=mem(100) #creating an arbitrary sized memory
 reg=mem(1,True) #creating a 32 bit register
 pc=0
 ir=[]
 
 #Reading the code
-add='Fibo.txt'
-with open(add,'rb') as f:
-    read=f.readlines()
-
-#Reading the code
 memory[0].pop()#cleaning the zero in register zero
 for i,entry in enumerate(read):
     memory[0]+=[entry.split()] #Loading and decoding the code into mem[0]
+
         
 
 while True:
     if not memory[0][pc]:
+        pc+=1
         pass
+    
     elif memory[0][pc][0]=='END':
         break
     else:
@@ -86,16 +153,17 @@ while True:
         a=int(ir[1])
         b=int(ir[2])
         c=int(ir[3])
+        #print memory[1:11]
+        #print reg
+        #print '--------------------'
         execute(op,a,b,c)
-    pc+=1 
-
-
-
+    print reg
 
 print memory #Outputting the final memory state
 print '\n\n'
-print memory[1:11] #plotting the first 10 Fiboncacci sequence numbers
-
+#print 'memory[4] is c, the answer'
+print memory[1:] #plotting the first 10 Fiboncacci sequence numbers
+print reg
 
 print 'DONE'
     
