60db393d725907a563e35f48ab92279a385cb840
plumSemPy <emin@berkeley.edu>
2017-07-05 15:09:16 -0700

    eight queens skeletal

---
+++ sicp/queens.scm
@@ -0,0 +1,19 @@
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+     	(list empty-board)
+     	(filter
+         (lambda (positions) (safe? k positions)) ;predicate
+          (flatmap                                ;sequence to to filter 
+            (lambda (rest-of-queens)
+              (map (lambda (new-row)
+                     (adjoin-position new-row k rest-of-queens))
+                   (enumerate-interval 1 board-size) 
+              )
+            )
+            (queens-cols (- k 1))
+          )
+     	)
+    )
+  (queen-cols board-size)
+)
